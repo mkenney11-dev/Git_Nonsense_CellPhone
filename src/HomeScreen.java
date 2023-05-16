f003bd9dcc54ccfa7d65743402ff1e41f4079d52
maggie <maggie.kenney115@gmail.com>
2023-05-16 10:25:27 -0400

    Home screen added

---
+++ src/HomeScreen.java
@@ -0,0 +1,9 @@
+public class HomeScreen {
+    //navigate to home screen
+    //apps on home screen
+    //settings on home screen
+
+
+    //change photo on background of home screen
+    //adjust home screen widgets
+}
